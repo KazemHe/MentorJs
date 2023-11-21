00df1ffdcee8b52e440993ff1896eeb4edb102d2
KazemHe <kazemhe1@gmail.com>
2023-11-22 00:20:36 +0200

    removing console logs

--- backend/services/socketService.js
+++ backend/services/socketService.js
@@ -7,7 +7,6 @@ module.exports = function (io) {
       rooms[room] = rooms[room] ? rooms[room] + 1 : 1;
       console.log(`User joined room: ${room}. Users in room: ${rooms[room]}`);
 
-      console.log("emit rooms", rooms, rooms[room] === 1);
       if (rooms[room] === 1) {
         socket.emit("is mentor", true);
       } else {
@@ -16,15 +15,8 @@ module.exports = function (io) {
     });
 
     socket.on("code editing", ( codeBlock) => {
-      //send other code editing to all users in room except sender
-      console.log('All rooms',rooms)
       Object.keys(rooms).forEach((roomId) => {
-          console.log('roomID befor the if',roomId)
         if (roomId !== "") {
-
-            console.log('roomID',roomId)
-            console.log('codeBlock',codeBlock)
-
           socket.to(roomId).emit("other code editing", codeBlock);
         }
       });
--- src/services/socketService.js
+++ src/services/socketService.js
@@ -9,27 +9,23 @@ export function useSocket(codeBlockId, setCodeBlock, setIsMentor,setEditedCode) 
     socketRef.current = io(serverUrl);
 
     socketRef.current.on('connect', () => {
-        console.log('onnected')
       socketRef.current.emit('join room', codeBlockId);
     });
 
     socketRef.current.on('is mentor', (isMentor) => {
-        console.log('recive is mentor')
       setIsMentor(isMentor);
     });
 
     socketRef.current.on('other code editing', (updatedCodeBlock) => {
-        console.log('outside the if',updatedCodeBlock)
       if (updatedCodeBlock._id === codeBlockId) {
-        console.log('inside the if')
         setEditedCode(updatedCodeBlock.code)
       }
     });
 
     return () => {
       socketRef.current.emit('leave room', codeBlockId);
-      console.log('disconnected')
       socketRef.current.disconnect();
+      console.log('disconnected')
     };
   }, [codeBlockId, setCodeBlock, setIsMentor,setEditedCode]);
 
